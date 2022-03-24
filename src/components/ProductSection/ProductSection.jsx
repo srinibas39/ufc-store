607e08926340b6d8ba5da81d794b08ba16c0e40f
srinibas <srinibaskhuntia39@gmail.com>
2022-03-24 07:41:47 +0530

    fetched all products

--- src/components/ProductSection/ProductSection.jsx
+++ src/components/ProductSection/ProductSection.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useState ,useEffect } from "react"
 import { AllProducts } from "../AllProducts/AllProducts"
 import { Filter } from "../Filter/Filter"
 
 export const ProductSection=()=>{
+    const [allProducts,setAllProducts]=useState(null);
+   
+    useEffect(()=>{
+      (async()=>{
+          try{
+            const res=await axios.get("/api/products",{
+                method:"GET"
+            })
+            setAllProducts(res.data.products);
+
+          }
+          catch(error){
+             console.log(error);
+          }
+       })()
+    },[])
     return  <section class="product-section">
         <Filter/>
         <AllProducts/>
--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,16 +1,77 @@
+import axios from "axios";
+import { useState } from "react";
+
 export const Signup = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  console.log(formData);
+
+  const handleSubmit = async (e) => {
+   e.preventDefault();
+    try {
+      const res = await axios.post(
+        "/api/auth/signup",
+        JSON.stringify(formData)
+      );
+      // save the JWT token in the server
+      localStorage.setItem("token",res.data.encodedToken)
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div class="login-container">
         <div class="login">
           <h2>Sign Up</h2>
           <div class="email-container">
+            <label for="firstName">First Name</label>
+            <input
+              type="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={(e) =>
+                setFormData({ ...formData, firstName: e.target.value })
+              }
+            />
+          </div>
+          <div class="email-container">
+            <label for="lastName">Last Name</label>
+            <input
+              type="lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={(e) =>
+                setFormData({ ...formData, lastName: e.target.value })
+              }
+            />
+          </div>
+          <div class="email-container">
             <label for="email">Email</label>
-            <input type="email" name="email" />
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+            />
           </div>
           <div class="password-container">
             <label for="password">Password</label>
-            <input type="password" name="password" />
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+            />
           </div>
           <div class="forgotP-container">
             <div class="remember">
@@ -18,7 +79,9 @@ export const Signup = () => {
               <label for="remember-me">I accept all terms and condition</label>
             </div>
           </div>
-          <button class="btn-login">Create new Account</button>
+          <button class="btn-login" onClick={() => handleSubmit()}>
+            Create new Account
+          </button>
           <div class="create-new">
             <a href="./Login.html" class="link link2"></a>
             <p>Already have an account</p>
