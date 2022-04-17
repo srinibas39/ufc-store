a42ab54ed79a3b552d71226a8240fc00001a592f
srinibas <srinibaskhuntia39@gmail.com>
2022-04-18 01:09:47 +0530

    signup implemented

--- src/context/AuthContext/AuthContext.js
+++ src/context/AuthContext/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext } from "react";
+import { useState } from "react/cjs/react.development";
 import { LoginService } from "../../services/LoginService";
 import { SignupServices } from "../../services/SignupServices";
 
@@ -6,29 +7,40 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
 
+    const [user, setUser] = useState("");
+    const [token, setToken] = useState("");
+
     const handleLogin = async (email, password) => {
         console.log({ email, password });
         try {
             const res = await LoginService({ email, password });
-            console.log(res);
+
+
+
         }
         catch (error) {
             console.log(error);
         }
     }
 
-    const handleSignup = async ( firstName, lastName, email, password ) => {
-        console.log({firstName, lastName, email, password});
-        try{
-            const res=await SignupServices({firstName, lastName, email, password});
-            console.log(res);
+    const handleSignup = async (firstName, lastName, email, password) => {
+
+        try {
+            const { data, status } = await SignupServices({ firstName, lastName, email, password });
+            if (status === 200 || status === 201) {
+                localStorage.setItem("login",
+                    JSON.stringify({ token: data.encodedToken, user: data.createdUser }))
+            }
+            setToken(data.encodedToken);
+            setUser(data.createdUser);
+            console.log("successfully signed in");
         }
-        catch(error){
+        catch (error) {
             console.log(error);
         }
     }
 
-    return <AuthContext.Provider value={{ handleLogin , handleSignup}}>
+    return <AuthContext.Provider value={{ handleLogin, handleSignup }}>
         {children}
     </AuthContext.Provider>
 }
