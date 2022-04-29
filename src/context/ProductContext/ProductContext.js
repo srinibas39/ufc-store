900805fd568f56825d59c023dd1911bcc139ff75
srinibas <srinibaskhuntia39@gmail.com>
2022-04-29 13:01:30 +0530

    Added cart functions to the productContext

--- src/context/ProductContext/ProductContext.js
+++ src/context/ProductContext/ProductContext.js
@@ -1,8 +1,10 @@
 import { createContext, useContext, useState, useEffect, useReducer } from "react";
+import { AddCart } from "../../services/AddCart";
 import { AddWishlist } from "../../services/AddWishlist";
-
 import { GetAllProducts } from "../../services/GetAllProducts";
+import { GetCart } from "../../services/GetCart";
 import { GetWishList } from "../../services/GetWishlist";
+import { RemoveCart } from "../../services/RemoveCart";
 import { RemoveWishlist } from "../../services/RemoveWishlist";
 import { useAuth } from "../AuthContext/AuthContext";
 
@@ -17,18 +19,18 @@ export const ProductProvider = ({ children }) => {
         switch (action.type) {
             case "GET_PRODUCTS":
                 return { ...state, allProducts: [...action.payload] }
-            case "GET_WISHLIST":
-                return { ...state, wishlist: [...action.payload] }
             case "ADD_REMOVE_WISHLIST":
                 return { ...state, wishlistItems: [...action.payload] }
+            case "ADD_REMOVE_CART":
+                return { ...state, cartItems: [...action.payload] }
             default:
                 return { ...state }
         }
     }
     const [prodState, prodDispatch] = useReducer(productReducer, {
         allProducts: [],
-        wishlist: [],
-        wishlistItems: []
+        wishlistItems: [],
+        cartItems: []
     })
     const getProduct = (productId) => prodState.allProducts.find((el) => el._id === productId) || {};
 
@@ -83,12 +85,56 @@ export const ProductProvider = ({ children }) => {
         }
     }
     const getWishlist = async (token) => {
-        const resWishlist = await GetWishList({ token });
-        if (resWishlist.status === 200 || resWishlist.status === 201) {
-            return resWishlist.data.wishlist;
+        try {
+
+            const resWishlist = await GetWishList({ token });
+            if (resWishlist.status === 200 || resWishlist.status === 201) {
+                return resWishlist.data.wishlist;
+            }
+        }
+        catch (error) {
+            console.log(error);
         }
     }
-    return <ProductContext.Provider value={{ prodState, getProduct, addWishlist, removeWishlist, toastLoading, setToastLoading, toastText, getWishlist }}>
+    const addCart = async (token, product) => {
+        try {
+            setToastLoading(true);
+            setToastText("Item is getting added in the cart")
+            const res = await AddCart(token, product);
+            if (res.status === 200 || res.status === 201) {
+                prodDispatch({ type: "ADD_REMOVE_CART", payload: res.data.cart })
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    const removeCart = async(token, id) => {
+        try {
+            setToastLoading(true);
+            setToastText("Item is getting removed from cart")
+            const res = await RemoveCart({ token, id })
+            if (res.status === 200 || res.status === 201) {
+                prodDispatch({ type: "ADD_REMOVE_CART", payload: res.data.cart })
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    const getCart = async(token) => {
+        try {
+            const res = await GetCart({ token });
+            if (res.status === 200 || res.status === 201) {
+                return res.data.cart
+            }
+
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    return <ProductContext.Provider value={{ prodState, getProduct, addWishlist, removeWishlist, toastLoading, setToastLoading, toastText, getWishlist, addCart, getCart, removeCart }}>
         {children}
     </ProductContext.Provider>
 }
