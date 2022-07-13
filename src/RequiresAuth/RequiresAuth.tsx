fd0590ab0de58a8f613f4f9088f46131bb4a9726
srinibas <srinibaskhuntia39@gmail.com>
2022-07-13 21:39:06 +0530

    added type to requiresAuth

--- src/RequiresAuth/RequiresAuth.tsx
+++ src/RequiresAuth/RequiresAuth.tsx
@@ -1,6 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom"
+import { RequiresAuthProps } from "./RequiresAuth.types";
 
-export const RequiresAuth = ({ token, children }) => {
+export const RequiresAuth = ({ token, children }:RequiresAuthProps) => {
     const location = useLocation();
     return token ? children : <Navigate to="/login" state={{ from: location }} replace />
 }
