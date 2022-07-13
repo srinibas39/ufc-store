d2c3646fbda3505265b0d1eecf2e74db8f2e7059
srinibas <srinibaskhuntia39@gmail.com>
2022-07-13 21:35:31 +0530

    added typescript to all the pages

--- src/pages/AddressPage.tsx
+++ src/pages/AddressPage.tsx
@@ -1,7 +1,7 @@
 import { Navbar } from "../components/NavBar/NavBar";
 import { Footer } from "../components/Footer/Footer";
-import { Address } from "../components/Address/Address.tsx";
 import { AutoComplete } from "../components/AutoComplete/AutoComplete";
+import { Address } from "../components/Address/Address";
 
 export const AddressPage = () => {
   return (
